9471d240a4ee32f2012c272dbad1ac9b1375937c
Jan Manrique <jan.manrique@est.fib.upc.edu>
2018-11-27 17:35:50 +0100

    Implemented all methods except recover

--- sample/bls_smpl.cpp
+++ sample/bls_smpl.cpp
@@ -27,59 +27,55 @@ int init()
 	return 0;
 }
 
-int sign(const std::string& m, const std::string& sKey, int id)
+int sign(const std::string& sKey, const std::string& msg)
 {
-	log("signing message: " << m);
+	log("signing message: " << msg);
 	bls::SecretKey sec;
 	set(sKey, sec);
 
 	bls::Signature s;
-	sec.sign(s, m);
+	sec.sign(s, msg);
 	std::cout << "sMsg: " << s << std::endl;
-	std::cout << "signed by : " << sec << std::endl;
 
 	return 0;
 }
 
-int verify(const std::string& m, const std::string& pKey, const std::string sMsg, int id)
+int verify(const std::string& pKey, const std::string& msg, const std::string sMsg)
 {
-	log("verify message " << m);
+	log("verify message " << msg);
 	bls::PublicKey pub;
 	set(pKey, pub);
 
 	bls::Signature s;
 	set(sMsg, s);
 
-	if (s.verify(pub, m)) {
-		puts("verify ok");
+	if (s.verify(pub, msg)) {
+		log("verify ok");
 		return 0;
 	} else {
-		puts("verify err");
+		log("verify err");
 		return 1;
 	}
 }
 
-int share(size_t n, size_t k)
+int share(const std::string& sKey, size_t k, const bls::IdVec& ids)
 {
+	size_t n = ids.size();
 	log((int)k << "-out-of-" << (int)n << " threshold sharing");
 	bls::SecretKey sec;
-	//load(sec, secFile);
+	set(sKey, sec);
 
 	bls::SecretKeyVec msk;
 	sec.getMasterSecretKey(msk, k);
 
 	bls::SecretKeyVec secVec(n);
-	bls::IdVec ids(n);
+	for (size_t i = 0; i < n; i++)
+		secVec[i].set(msk, ids[i]);
+
 	for (size_t i = 0; i < n; i++) {
-		int id = i + 1;
-		ids[i] = id;
-		secVec[i].set(msk, id);
-	}
-	for (size_t i = 0; i < n; i++) {
-		//save(secFile, secVec[i], ids[i]);
 		bls::PublicKey pub;
 		secVec[i].getPublicKey(pub);
-		//save(pubFile, pub, ids[i]);
+		std::cout << "share-" << ids[i] << ": sk=" << secVec[i] << " pk=" << pub << std::endl;
 	}
 
 	return 0;
@@ -115,14 +111,12 @@ int main(int argc, char *argv[])
 	std::string sMsg;
 	std::string sKey;
 	std::string pKey;
-	size_t n;
 	size_t k;
 	int id;
 	bls::IdVec ids;
 
 	cybozu::Option opt;
 	opt.appendParam(&mode, "init|sign|verify|share|recover");
-	opt.appendOpt(&n, 10, "n", ": k-out-of-n threshold");
 	opt.appendOpt(&k, 3, "k", ": k-out-of-n threshold");
 	opt.appendOpt(&sKey, "", "sk", ": secret key");
 	opt.appendOpt(&pKey, "", "pk", ": public key");
@@ -138,21 +132,21 @@ int main(int argc, char *argv[])
 	if (mode == "init") {
 		return init();
 	} else if (mode == "sign") {
-		if (msg.empty()) goto ERR_EXIT;
 		if (sKey.empty()) goto ERR_EXIT;
-		return sign(msg, sKey, id);
-	} else if (mode == "verify") {
 		if (msg.empty()) goto ERR_EXIT;
+		return sign(sKey, msg);
+	} else if (mode == "verify") {
 		if (pKey.empty()) goto ERR_EXIT;
+		if (msg.empty()) goto ERR_EXIT;
 		if (sMsg.empty()) goto ERR_EXIT;
-		return verify(msg, pKey, sMsg, id);
+		return verify(pKey, msg, sMsg);
 	} else if (mode == "share") {
-		return share(n, k);
+		if (sKey.empty()) goto ERR_EXIT;
+		if (!k) goto ERR_EXIT;
+		if (ids.empty()) goto ERR_EXIT;
+		return share(sKey, k, ids);
 	} else if (mode == "recover") {
-		if (ids.empty()) {
-			fprintf(stderr, "use -ids option. ex. share -ids 1 3 5\n");
-			goto ERR_EXIT;
-		}
+		if (ids.empty()) goto ERR_EXIT;
 		return recover(ids);
 	} else {
 		fprintf(stderr, "bad mode %s\n", mode.c_str());
