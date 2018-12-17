801ad24d09966730dfd9f318b2000820c62e58b7
Jan Manrique <jan.manrique@est.fib.upc.edu>
2018-12-17 20:11:44 +0100

    Updated getDependencies script to set always same mcl version

--- getDependencies.sh
+++ getDependencies.sh
@@ -14,4 +14,7 @@ dir="$(dirname $0)/../mcl"
 if [ ! -d "$dir" ]; then
   # Download mcl
   git clone https://github.com/herumi/mcl.git "$dir"
+
+  cd "$dir" > /dev/null 
+  git checkout 5a16675 > /dev/null 2>&1
 fi
